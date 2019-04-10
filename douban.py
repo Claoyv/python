910835a18e1f67cb731d436aa883df3d97ebf61c
Claoyv <claoyv@163.com>
2019-04-10 09:32:31 +0800

    get douban index page. do.

--- douban.py
+++ douban.py
@@ -1,6 +1,7 @@
 import requests,time
 from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
+import pymongo
 
 url='https://book.douban.com/tag/?view=type&icn=index-sorttags-all'
 headers = {
@@ -12,6 +13,12 @@ headers = {
     'Referer':'http://www.baidu.com'
     }
 
+
+client = pymongo.MongoClient('80.80.1.80',27017)
+db_spider = client['spider']
+page_douban = db_spider['douban']
+
+
 def get_page(url):
     try:
         res = requests.get(url,headers=headers)
@@ -21,15 +28,17 @@ def get_page(url):
     except RequestException:
         return None
 
-def parse_page(html):
+def parse_index_page(html):
     soup = BeautifulSoup(html,'lxml')
     tbs = soup.select("#content > div > div.article > div:nth-of-type(2) > div > table > tbody > tr > td")
     for tb in tbs:
-        print(tb.get_text())
-
+        page_douban.insert_one({'page' : tb.get_text(),'link' : tb.a.get('href')})
+        print("Get down page"+tb.get_text())    
+#    return data     
+ 
 def main():
     html = get_page(url)
-    parse_page(html)
+    parse_index_page(html)
 
 if __name__=="__main__":
     main()
